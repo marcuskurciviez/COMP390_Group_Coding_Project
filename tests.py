ab7260545a4e23d18387bb56dc9f60e99de27ba8
Kauetheadventurer246 <kauetheadventurer246@gmail.com>
2022-12-22 12:59:56 -0500

    apple

--- tests.py
+++ tests.py
@@ -1,17 +1,37 @@
 import pytest
 from io import StringIO
 from testfixtures import TempDirectory
-
 import print_funcs
+import util_functions
 from print_funcs import opening_statement
 
 
-def test_opening_statement(monkeypatch, capfd):
-    test_string = ""
-    simulated_input = StringIO(test_string)
-    monkeypatch.setattr('sys.stdin', simulated_input)
-    assert print_funcs.opening_statement() is None
-    out, err = capfd.readouterr()
-    assert out == "1"
+def test_string_is_int(capfd):
+    assert util_functions._string_is_int('104') is True
+    assert util_functions._string_is_int('-35') is True
+    assert util_functions._string_is_int(12537) is True
+    assert util_functions._string_is_int(123.456) is True
+    assert util_functions._string_is_int(2/3) is True
+    assert util_functions._string_is_int('one') is False
+    assert util_functions._string_is_int('Kratos') is False
+    assert util_functions._string_is_int('') is False
 
+
+def test_convert_string_to_numerical(capfd):
+    assert util_functions.convert_string_to_numerical('12.0') == 12
+    assert util_functions.convert_string_to_numerical('0.001') == 0.001
+    assert util_functions.convert_string_to_numerical('Atreus') is None
+
+
+def test_string_is_float(capfd):
+    assert util_functions._string_is_float('12.0') is True
+    assert util_functions._string_is_float('12') is True
+    assert util_functions._string_is_float('0') is True
+    assert util_functions._string_is_float('-0.123') is True
+    assert util_functions._string_is_float('-0') is True
+    assert util_functions._string_is_float('Do not trust Odin') is False
+    assert util_functions._string_is_float('12.12.134.233') is False
+    assert util_functions._string_is_float('5/2') is False
+    assert util_functions._string_is_float('apple.cider.is.good') is False
+    assert util_functions._string_is_float('(12/2)/2') is False
 

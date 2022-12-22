e9db284c0ccf4f0244f5003ae7f98fafca74644c
Kauetheadventurer246 <kauetheadventurer246@gmail.com>
2022-12-21 22:35:10 -0500

    apple

--- tests.py
+++ tests.py
@@ -2,13 +2,15 @@ import pytest
 from io import StringIO
 from testfixtures import TempDirectory
 
+import print_funcs
 from print_funcs import opening_statement
 
 
-def test_print_functions(monkeypatch, capfd):
-    test_string = "1"
+def test_opening_statement(monkeypatch, capfd):
+    test_string = ""
     simulated_input = StringIO(test_string)
     monkeypatch.setattr('sys.stdin', simulated_input)
+    assert print_funcs.opening_statement() is None
     out, err = capfd.readouterr()
     assert out == "1"
 
